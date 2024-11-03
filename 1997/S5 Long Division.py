16d0a34bfbb332dab5ba6b49d38477f5d502a03b
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2024-11-03 10:20:04 -0500

    1997 S5

---
+++ 1997/S5 Long Division.py
@@ -0,0 +1,19 @@
+# https://dmoj.ca/problem/ccc97s5
+# Yeah I know I'm not supposed to use Python, I'm supposed to use strings or whatever, I don't care
+
+
+def main():
+    """Process multiple test cases of division."""
+    test_cases = int(input())
+
+    for _ in range(test_cases):
+        dividend = int(input())
+        divisor = int(input())
+
+        print(dividend // divisor)
+        print(dividend % divisor)
+        print()
+
+
+if __name__ == "__main__":
+    main()
